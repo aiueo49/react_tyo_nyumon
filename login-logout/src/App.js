db3e9111f54839fbb41b0a47213addf0df634904
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-06-07 01:16:55 +0900

    🚧 ToStringは組み込み関数。{}の中はJSとして評価される。

--- login-logout/src/App.js
+++ login-logout/src/App.js
@@ -18,6 +18,24 @@ function App() {
     <div className="container">
       <div className="content">
         {/* ログイン状態に応じて表示を切り替える */}
+        {isLoggedIn ? (
+          {/* ログイン状態の場合 */}
+          <div>
+            <h1 className="title">ログイン済みです</h1>
+            <button className="button" onClick={handleLogout}>
+              ログアウト
+            </button>
+          </div>
+          ) : (
+          {/* ログアウト状態の場合 */}
+          <div>
+            <h1 className="title">ログインしてください</h1>
+            <button className="button" onClick={handleLogin}>
+              ログイン
+            </button>
+          </div>
+        )}
+        <span>isLoggeIn: {isLoggedIn.toString()}</span>
       </div>
     </div>
  );
