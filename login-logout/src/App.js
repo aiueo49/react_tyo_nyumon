bb940859769d1a388b2da5a7c2fd388592254e3d
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-06-07 00:48:54 +0900

    🚧 Falseはログインしていない状態

--- login-logout/src/App.js
+++ login-logout/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="content">
+        {/* ログイン状態に応じて表示を切り替える */}
+      </div>
     </div>
-  );
+ );
 }
 
 export default App;
