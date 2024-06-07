ea5af63b29cbed1f264f5dc45deb175831106dcb
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-06-07 15:08:02 +0900

    ✨ リストを表示

--- shopping-list/src/App.js
+++ shopping-list/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
 function App() {
+  // ショッピングリスト
+  const shoppingList = [
+    { id: 1, name: 'Reactシューズ 20XX限定バージョン'},
+    { id: 2, name: 'Reactソーダ React Tシャツ オレンジ味'},
+    { id: 3, name: 'React Tシャツ Mondayモデル'},
+  ];
+  // map関数
+  const listItems = shoppingList.map((item) => (
+    <li key={item.id}>{item.name}</li>
+  ));
+
   return (
-    <div>
-      {/* ショッピングリスト */}
+    <div className="container">
+      <ul>{listItems}</ul>
     </div>
  );
 }
